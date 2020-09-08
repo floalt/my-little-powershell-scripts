db968b725c5fe69aa45a6c880f72b823a67a42ff
floalt <flo.alt@fa-netz.de>
2020-09-08 20:50:05 +0200

    new script: Set-MailboxLanguage

---
+++ set-MailboxLanguage.ps1
@@ -0,0 +1,31 @@
+﻿# Parameter
+
+param(
+[string]$mailbox
+)
+
+# Check Usage
+
+if ($mailbox -eq "") { Write-Host "
+
+    Stellt die Ordnersprache von Office365-Postfächern auf 'deutsch' um.
+
+    Verwendung: ./set-MailboxLanguage -mailbox <mailbox>
+
+"
+exit 0
+}
+
+echo "Mailbox: $mailbox"
+
+# Verbindung mit Office365 herstellen
+
+$LiveCred = Get-Credential    # hier Anmeldedaten vom Office365-Admin in PopUp-Fenster eingeben
+$Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://ps.outlook.com/powershell/ -Credential $LiveCred -Authentication Basic -AllowRedirection
+Import-PSSession $Session
+Connect-MsolService –Credential $LiveCred
+
+# Ordnersprache ändern
+
+Set-MailboxRegionalConfiguration $mailbox -Language de-DE -LocalizeDefaultFolderName
+Get-MailboxRegionalConfiguration $mailbox

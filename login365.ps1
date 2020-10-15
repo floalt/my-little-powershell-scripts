5237a2a0f0f571fe97a19e8d2e6adada49b7efa2
floalt <flo.alt@fa-netz.de>
2020-10-15 12:01:05 +0200

    change connection uri

--- login365.ps1
+++ login365.ps1
@@ -1,6 +1,6 @@
 ﻿# Verbindung mit Office365 herstellen
 
 $LiveCred = Get-Credential    # hier Anmeldedaten vom Office365-Admin in PopUp-Fenster eingeben
-$Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://ps.outlook.com/powershell/ -Credential $LiveCred -Authentication Basic -AllowRedirection
-Import-PSSession $Session
+$Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell/ -Credential $LiveCred -Authentication Basic -AllowRedirection
+Import-PSSession $Session -AllowClobber
 Connect-MsolService –Credential $LiveCred

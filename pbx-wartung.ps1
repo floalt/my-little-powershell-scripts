e2473b6cd8a300efc7f0aed9d69a8bce5c04f529
floalt <flo.alt@fa-netz.de>
2021-01-22 11:32:29 +0100

    Added BPX UC-Server Wartungsscript

---
+++ pbx-wartung.ps1
@@ -0,0 +1,19 @@
+﻿# Wartungsscript für UC Server
+# author: flo.alt@fa-netz.de
+# Ver 0.6
+
+$service = "PBX Call Assist"
+
+$delpath = "C:\Program Files\Auerswald\UCServer\logs"
+$Daysback = "-21"
+
+
+# Logfiles löschen, älter als $daysback
+
+$CurrentDate = Get-Date
+$DatetoDelete = $CurrentDate.AddDays($Daysback)
+Get-ChildItem $delpath | Where-Object { $_.LastWriteTime -lt $DatetoDelete } | Remove-Item
+
+
+# Dienst neu starten
+Get-Service -DisplayName "*$service*" | Restart-Service

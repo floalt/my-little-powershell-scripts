030f55e669ea1f2106e687429b1f6a596c94f931
floalt <flo.alt@fa-netz.de>
2021-11-18 17:10:02 +0100

    description at netusecreds.ps1

--- netusecreds.ps1
+++ netusecreds.ps1
@@ -1,17 +1,21 @@
 ﻿<#
-this scripts add login credentials in local store
+description:
 
-You have to do this:
- - make a GPO that maps the desired network drive
- - Add to this GPO the option to set a RUN-Value in registry
-   (only if $env:APPDATA\atriumcreds doesnt exists)
-   This RUN-Value will start this script
+    this script adds login credentials in local store
 
-And so it is running
- 1. The RUN-Value starts this script on Logon
- 2. The file $env:APPDATA\atriumcreds is created
- 3. THE RUN-Value will be deleted
- 4. On next User Logon, the GPO mapping the network drive will be working
+    You have to do this:
+    - make a GPO that maps the desired network drive
+    - Add to this GPO the option to set a RUN-Value in registry
+    (only if $env:APPDATA\atriumcreds doesnt exists)
+    This RUN-Value will start this script. Include the parameters within the RUN-Value.
+
+    And thats how it runs:
+    1. The RUN-Value starts this script on Logon
+    2. The file $env:APPDATA\netusecreds is created
+    3. THE RUN-Value will be deleted
+    4. On next User Logon, the GPO mapping the network drive will be working
+
+author: flo.alt@fa-netz.de
 
 #>
 

c033bd8996cd975ffff7b297be056de856e331af
floalt <flo.alt@fa-netz.de>
2022-06-01 20:36:13 +0200

    bitlocker: logfile added

--- bitlocker-enable.ps1
+++ bitlocker-enable.ps1
@@ -3,41 +3,19 @@ description
 
     Checks the bitlocker state of local hard disk
     If it is not encrypted: encryption starts
-    Saves key in Active Directory
+    Saves key in Active Directory (depends on domain GPO)
 
 author: flo.alt@fa-netz.de
 
 #>
 
-# ------------------------ FUNCTIONS ------------------------ #
+# Definitions & functions
 
-function errorcheck {
+    $logfile = "C:\Windows\Temp\bitlocker-enable.log"
 
-    <#
-    writing $yeah and $shit in standard output and counting errors
-
-    usage:
-        declare at the beginning of your script: $errorcount = 0
-        set '-ErrorVariable errchk' to every cmdlet you want to errorcheck
-        $yeah = "OK: everything went allright"
-        $shit = "ERROR: this didnt work"
-        [do someting complicated]; errorcheck
-    #>
-
-    if (!$errchk) {
-        write-host $yeah -F Green
-    } else {
-        write-host $shit -F Red
+    function start-logfile {
+        "Beginning: $(Get-Date -Format yyyy-MM-dd_HH:mm:ss)" >> $logfile
     }
-
-    $errchk = $null
-}
-
-
-
-
-
-# ------------------------ Here is the Script ------------------------ #
 
 
 # Checks the bitlocker state of local hard disk
@@ -46,21 +24,30 @@ function errorcheck {
 
     switch ($encryptvolume.VolumeStatus) {
         FullyEncrypted {
-            write-host "INFO: Local Disk is already fully encrpyted. Doing nothing."
+            start-logfile
+            "INFO: Local Disk is already fully encrpyted. Doing nothing." >> $logfile
             break
         }
         EncryptionInProgress {
+            start-logfile
             $percentage = ($encryptvolume.EncryptionPercentage)
-            write-host "INFO: Encryption is in progress: $percentage%"
+            "INFO: Encryption is in progress: $percentage%" >> $logfile
             break
         }
         FullyDecrypted {
-            write-host "OK: Local Disk is NOT encrpyted. Enabling Bitlocker now..."
-            $do_encrypt = "pending"
+            if (test-path $logfile) {
+                "INFO: Encryption is enabled, but waiting for reboot to start encrypting" >> $logfile
+            }
+            else {
+                start-logfile
+                "OK: Local Disk is NOT encrpyted. Enabling Bitlocker now..." >> $logfile
+                $do_encrypt = "pending"
+            }
             break
         }
         Default {
-            Write-Host "FAIL: Something strange has happened. Cannot decide if it is a good idea to start encryption right now."
+            start-logfile
+            "FAIL: Something strange has happened. Cannot decide if it is a good idea to start encryption right now." >> $logfile
             break
         }
     }
@@ -76,7 +63,7 @@ function errorcheck {
 
         $check_usb = (GET-WMIOBJECT win32_diskdrive | where { $_.InterfaceType -eq "USB" })
         if ($check_usb) {
-            write-host "WARNING: There is a USB-Stick connected. I better do not enable Bitlocker"
+            "WARNING: There is a USB-Stick connected. I better do not enable Bitlocker" >> $logfile
             exit
         }
 
@@ -84,7 +71,11 @@ function errorcheck {
 
         $yeah = "OK: Bitlocker is now enabled. Waiting for reboot to start encryption"
         $shit = "ERROR: Could not enable Bitlocker"
-        $encryptvolume | Enable-BitLocker -EncryptionMethod Aes256 -RecoveryPasswordProtector -ErrorVariable errchk; errorcheck
+        $encryptvolume | Enable-BitLocker -EncryptionMethod Aes256 -RecoveryPasswordProtector
+
+        # set marker for waiting-for-reboot
+
+        "Waiting for reboot" >> $logfile
         
     }
 
